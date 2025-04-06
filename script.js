232aa3ab6daf8ac578969ca36e235810f4a72489
Enoocdev <enooclucen@gmail.com>
2025-04-06 19:04:51 +0200

    mejoras

--- script.js
+++ script.js
@@ -16,9 +16,9 @@ function cambiarUrl(){
                 let np = `?page=${pagina}`;
                 let news = document.querySelector("#news").checked == true ?  `&status=upcoming` : "";
                 let gener = generes != "" ? "&genres=" + generes : ""
-                let orderBy = order != "" ? `&order_by=` +  order : ""
+                let orderBy = order != "" ? `&order_by=` +  order + "&sort=desc": ""
 
-            return (jikan + tipo + np + news + orderBy + "&sort=desc"  + gener);
+            return (jikan + tipo + np + news + orderBy   + gener);
         }
 
 window.onload = async function (dentroTipo) {
